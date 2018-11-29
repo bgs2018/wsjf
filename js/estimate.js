9a083764195768ea652f62d586a2ed45b5f50056
azp74 <azp74@hotmail.com>
2018-11-29 13:11:32 +1030

    Update estimate.js
    Remove fields we don't use & update wsjf calculation

--- js/estimate.js
+++ js/estimate.js
@@ -13,17 +13,10 @@ var warning = document.getElementById('warning');
 document.getElementById('save').addEventListener('click', function(){
   return t.set('card', 'shared', 'ubv', ubvSelector.value)
   .then(function(){
-    return t.set('card', 'shared',  'tc', tcSelector.value)
-  })
-  .then(function(){
-    return t.set('card', 'shared',  'rroe', rroeSelector.value)
-  })
-  .then(function(){
     return t.set('card', 'shared',  'js', jsSelector.value)
   })
   .then(function(){
-    
-    var wsjfval = ((ubvSelector.value+tcSelector.value+rroeSelector.value)/jsSelector.value)
+    var wsjfval = (ubvSelector.value/jsSelector.value)
     return t.set('card', 'shared',  'wsjf', (parseFloat(Math.round(wsjfval) * 100) / 10000).toFixed(2))
   
   })
@@ -39,17 +32,13 @@ var wsjf = 0;
 t.render(function(){
   return Promise.all([
     t.get('card', 'shared', 'ubv'),
-    t.get('card', 'shared', 'tc'),
-    t.get('card', 'shared', 'rroe'),
     t.get('card', 'shared', 'js'),
     t.get('card', 'shared', 'wsjf'),
     
     ])
-  .spread(function(savedUbv, savedTc, savedRroe, savedJs, savedWsjf){
+  .spread(function(savedUbv, savedWsjf){
     
      ubvSelector.value = savedUbv;
-     tcSelector.value = savedTc;
-     rroeSelector.value = savedRroe;
      jsSelector.value = savedJs;
      wsjfCalc.innerHTML = savedWsjf; 
     
