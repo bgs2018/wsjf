e2afee2cf4214606016b66f58e0d2b688eca5c13
azp74 <azp74@hotmail.com>
2018-12-04 11:25:07 +1030

    Update estimate.js

--- js/estimate.js
+++ js/estimate.js
@@ -16,7 +16,7 @@ document.getElementById('save').addEventListener('click', function(){
   .then(function(){
     var wsjfval = (ubvSelector.value/jsSelector.value)
     /*return t.set('card', 'shared',  'wsjf', (parseFloat(Math.round(wsjfval) * 100) / 10000).toFixed(2))*/
-    return t.set('card', 'shared',  'wsjf', wsjfval)
+    return t.set('card', 'shared',  'wsjf', wsjfval*100)
   
   })
   .then(function(){
